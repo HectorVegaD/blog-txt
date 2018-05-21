06b68e51eb722246578274d51ed4744ce03222ba
HectorVegaD <Hector.DeLaVega@student.csulb.edu>
2018-05-20 23:20:34 -0700

    Removed unnecessary code

--- server.js
+++ server.js
@@ -20,9 +20,6 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'client/index.html'));
 });
 
-app.get('/', (req, res) => {
-    res.send('Hello World! - TXT BLOG ALIVE!');
-});
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Listening on port ${PORT}... `));
